ea7d7fb7b9b3cdc625cf74ab4eba1d1abd638aee
spazefalcon <88969+netcinematics@users.noreply.github.com>
2025-11-18 20:28:52 -0800

    Update install.py

--- install.py
+++ install.py
@@ -1,14 +1,8 @@
-print("inside")
 import subprocess
 import sys
 
-# is_colab = "google.colab" in sys.modules
+# example code for kaggle import
 is_kaggle = "kaggle_secrets" in sys.modules
-print(is_kaggle)
-# torch-scatter binaries depend on the torch and CUDA version, so we define the
-# mappings here for Colab & Kaggle
-# torch_to_cuda = {"1.10.0": "cu113", "1.9.0": "cu111", "1.9.1": "cu111"}
-
 
 def install_requirements():
     print("⏳ Installing requirements ...")
